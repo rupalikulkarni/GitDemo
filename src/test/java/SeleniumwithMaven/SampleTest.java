4298174fcc00efbb54b2c4434ca1e5f92f201a6a
rupali <rupali.mitm13@gmail.com>
2019-03-04 18:05:35 -0500

    first commit

---
+++ src/test/java/SeleniumwithMaven/SampleTest.java
@@ -0,0 +1,13 @@
+package SeleniumwithMaven;
+
+import org.testng.annotations.Test;
+@Test
+public class SampleTest {
+ 
+	public void BrowserAutomation()
+	{
+		System.out.println("Browser testing");
+
+	}
+
+}
